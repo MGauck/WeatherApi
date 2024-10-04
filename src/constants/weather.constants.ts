4bd470fb282743358025615866e67a211fab9dd9
Michal Galka <michal.g.galka@gmail.com>
2024-10-04 15:10:28 -0600

    2/3 functionallity

---
+++ src/constants/weather.constants.ts
@@ -0,0 +1,8 @@
+export const COLD_THRESHOLD = 283.15;
+export const HOT_THRESHOLD = 293.15;
+   
+export enum TEMPERATURE {
+   IS_COLD = "Is Cold",
+   IS_MODERATE = "Is Moderate",
+   IS_HOT = "Is Hot",
+}
