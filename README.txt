5357fcf3940e80bb64c796de5a0bfd0cfbb2052a
Michal Galka <michal.g.galka@gmail.com>
2024-10-04 20:35:41 -0600

    readme

---
+++ README.txt
@@ -0,0 +1,7 @@
+Basic Usage:
+
+1. Paste API key sent to you in email, in the file src\app\controllers\weather.controller.ts, line: 8
+2. npm run dev
+3. http://localhost:4000/weather?lat={lat}long={long}
+  Remember that both lat and long must be correct values, otherwise you will get 400 response
+4. For swagger documentation, go to: http://localhost:4000/api-docs/
--- src/app/controllers/weather.controller.ts
+++ src/app/controllers/weather.controller.ts
@@ -4,6 +4,8 @@ import { HTTP_CODES } from "../constants/httpsCodes.constants";
 import { TEMPERATURE, WEATHER_API_URL } from "../constants/weather.constants";
 import { getWeatherFeels } from "../utils/weather.utils";
 
+// Please paste here API that you get by email. Normally this key would be read from some cloud storage
+// like Azure Key Vault
 const APP_ID = "";
 
 export interface IWeather {
@@ -16,6 +18,8 @@ export const getWeather = async (req: Request, res: Response) => {
   const { lat, long } = req.query;
 
   if (isValidLatitude(lat) && isValidLongitude(long)) {
+    // Using One Call API 3.0, would allow me to exclude unnecessary fields, to minimize data size
+    // Unfortunatelly, it requires payment, and free API 2.5 does not have this option
     const response = await fetch(`${WEATHER_API_URL}?lat=${lat}&lon=${long}&appid=${APP_ID}`);
 
     if (!response.ok) {
