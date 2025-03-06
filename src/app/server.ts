b7fdf86ef80520d7ffb4562167443a057b592ebe
Michal Galka <michal.g.galka@gmail.com>
2025-03-05 17:16:51 -0700

    revert changes

--- src/app/server.ts
+++ src/app/server.ts
@@ -16,7 +16,7 @@ const server = app.listen({ port: PORT }, () => {
   console.log(`Server running on port ${PORT}`);  
   console.log(`My changes 1 ${PORT}`);  
   console.log(`My changes 2 ${PORT}`);  
-  console.log(`My changes 3 ${PORT}`);  
+  console.log(`My changes 4 ${PORT}`);  
 });
 
 export default server;
